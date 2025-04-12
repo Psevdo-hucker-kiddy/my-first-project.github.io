de0eecc7c4384338c8658fe68145f97b83272933
Psevdo-hucker-kiddy <153774803+Psevdo-hucker-kiddy@users.noreply.github.com>
2025-04-12 16:59:26 +0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo $pageTitle; ?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="about.php">О нас</a></li>
+        <li><a href="contact.php">Контакты</a></li>
+        <li><a href="login.php">Вход</a></li>
+        <li><a href="register.php">Регистрация</a></li>
+      </ul>
+    </nav>
+  </header>
